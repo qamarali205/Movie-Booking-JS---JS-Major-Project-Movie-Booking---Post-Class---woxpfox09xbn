8f5944a71ecc5168acc0f3454757077d9813d1e5
qamarali205 <qamarali205@gmail.com.com>
2023-02-10 02:02:10 +0530

    update

--- app.js
+++ app.js
@@ -1,2 +1,41 @@
 import {fetchMovieAvailability,fetchMovieList} from "./api.js"
-console.log(fetchMovieAvailability);
+
+
+
+const movieLinks = document.querySelectorAll('.movie-link');
+const booker = document.querySelector('#booker');
+const gridHolder = document.querySelector('#booker-grid-holder');
+const bookTicketBtn = document.querySelector('#book-ticket-btn');
+const confirmPurchase = document.querySelector('#confirm-purchase');
+const customerDetailForm = document.querySelector('#customer-detail-form');
+const success = document.querySelector('#success');
+
+// Get movie availability data and render seat grids
+movieLinks.forEach(link => {
+  link.addEventListener('click', async e => {
+    e.preventDefault();
+
+    const movieName = e.target.dataset.d;
+    const availability = await fetchMovieAvailability(movieName);
+
+    renderSeatGrids(availability);
+    booker.classList.remove('v-none');
+  });
+});
+
+// Handle seat selection
+gridHolder.addEventListener('click', e => {
+  if (e.target.classList.contains('available-seat')) {
+    e.target.classList.toggle('selected-seat');
+
+    const selectedSeats = document.querySelectorAll('.selected-seat');
+
+    if (selectedSeats.length) {
+      bookTicketBtn.classList.remove('v-none');
+    } else {
+      bookTicketBtn.classList.add('v-none');
+    }
+  }
+});
+
+//
